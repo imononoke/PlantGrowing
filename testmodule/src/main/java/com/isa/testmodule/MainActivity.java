91a9be246d9c247a77ff799b03cfab127abd2ee5
Jiazheng Sun <jiazheng.sun@dianrong.com>
2016-07-15 11:47:36 +0800

    init plant growing repo

---
+++ testmodule/src/main/java/com/isa/testmodule/MainActivity.java
@@ -0,0 +1,15 @@
+package com.isa.testmodule;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dianrong.testmodule.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
